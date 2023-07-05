05317b41456c7b83b23e13b8dce7c161dcd25546
gedons <gedoniani@gmail.com>
2023-07-05 10:08:04 +0100

    login and register logo fixed

--- src/components/MainLayout.vue
+++ src/components/MainLayout.vue
@@ -5,7 +5,7 @@
         <div class="flex h-16 items-center justify-between">
           <div class="flex items-center">
             <div class="flex-shrink-0">
-              <img class="h-8 w-8" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+              <img class="h-8 w-8" src="/task.svg"
                 alt="Your Company" />
             </div>
             <div class="hidden md:block">
